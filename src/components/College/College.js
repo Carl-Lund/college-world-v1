8f9d705e80fc425644b826e9da4bc93b17ee039e
allanbrock <abrocken@endicott.edu>
2019-10-02 15:52:53 -0400

    Cleaned up College.js code.

--- src/components/College/College.js
+++ src/components/College/College.js
@@ -4,15 +4,18 @@ import Navigation from "../Navigation/Navigation";
 export default class College extends React.Component {
     constructor(props) {
         super(props);
+
+        // The state hold information about the college retrieved from College World App
         this.state = {
-            data: null,
+            collegeData: null,
         };
     }
     componentDidMount() {
+        // Fetch data through the REST College World interface
         fetch('http://localhost:8080/enccollegeworld_war_exploded/rest/college/acorn')
             .then(response => response.json())
-            .then(data => {this.setState({ data });
-                console.log("Fetched college data " + data)
+            .then(collegeData => {this.setState({ collegeData });
+                console.log("Fetched college data " + collegeData)
             });
     }
 
@@ -21,16 +24,12 @@ export default class College extends React.Component {
             <div>
                 <h2>Start of college view...</h2>
                 <Navigation />
-                {this.state.data && (
-                    <h2>College balance = {this.state.data.availableCash}</h2>
+                {this.state.collegeData && (
+                    <h2>College balance = {this.state.collegeData.availableCash}</h2>
                     )
                 }
 
             </div>
         );
-        /*
-        <Navigation />
-        return <h2>This is the overall college view.</h2>;
-        */
     }
 }
