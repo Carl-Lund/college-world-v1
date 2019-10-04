3717089cfa8aaaa71c7784dc0163314219306d79
allanbrock <abrocken@endicott.edu>
2019-10-04 19:42:55 -0400

    Added routing for Store.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Students from "./components/Students/Students";
 import Sports from "./components/Sports/Sports";
 import Objectives from "./components/Objectives/Objectives";
 import Faculty from "./components/Faculty/Faculty";
+import Store from "./components/Store/Store";
 //import Buildings from "./components/Buildings/Faculty";
 
 ReactDOM.render(
@@ -23,25 +24,3 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root'));
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-// import College from "./components/college-view/college-view";
-// import Students from "./components/students-view/students-view";
-//
-// ReactDOM.render(<College />, document.getElementById('root'));
-// ReactDOM.render(<Students />, document.getElementById('root'));
-
-// Original default app code that react created.
-// Leaving this in case we need to reference it.
-//
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
-// class Navigation extends React.Component {
-//     render() {
-//         return <h2>This is the navigation bar</h2>;
-//     }
-// }
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
