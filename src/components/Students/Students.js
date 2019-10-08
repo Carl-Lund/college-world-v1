c1c1fc81cf01bba83fb6cbd3b7615779146eff89
timajmello <timjamello@icloud.com>
2019-10-08 16:26:37 -0400

    added students table component

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,36 +1,12 @@
 import React from 'react';
+import StudentsTable from "./StudentsTable";
 
 export default class Students extends React.Component {
 
     render() {
-        let table = [];
-
-        let children = [];
-        children.push(<td>NAME</td>)
-        children.push(<td>ID NUMBER</td>)
-        children.push(<td>HAPPINESS</td>)
-        children.push(<td>ATHLETE</td>)
-        children.push(<td>ATHLETIC ABILITY</td>)
-        table.push(<tr>{children}</tr>)
-
-
-        for (let i = 0; i < 5; i++) {
-            children = [];
-            // column by column
-            children.push(<td>{this.props.everything.students[i].name}</td>)
-            children.push(<td>{this.props.everything.students[i].idNumber}</td>)
-            children.push(<td>{this.props.everything.students[i].happinessLevel}</td>)
-            children.push(<td>{this.props.everything.students[i].athlete.toString()}</td>)
-            children.push(<td>{this.props.everything.students[i].athleticAbility}</td>)
-            // row by row
-            table.push(<tr>{children}</tr>)
-        }
-
         return (
             <div>
-                <table>
-                    {table}
-                </table>
+                <StudentsTable everything = {this.props.everything}/>
             </div>
         );
     }
