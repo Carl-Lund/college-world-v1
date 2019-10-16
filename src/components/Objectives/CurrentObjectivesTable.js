a8a6c17bdf5db4d6cefc4ca592b861ced2cd89fa
ebawmann <ebawm372@mail.endicott.edu>
2019-10-16 13:06:45 -0400

    Adds a CurrentLevel component.

--- src/components/Objectives/CurrentObjectivesTable.js
+++ src/components/Objectives/CurrentObjectivesTable.js
@@ -24,6 +24,19 @@ function createTable(data) {
     for(let i =1; i <=5; i++) {
         if (i <= data.currentLevel) continue;
         table.push(<h3>Level {i}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{data.studentsNeededForLevel[i]} Students</h3>);
+
+        for(let j =0; j<=data.gates.length; j++) {
+            if (i == data.gates[j].level) {
+                table.push(<li className="list-group-item">
+                    <div className="col-md-2" style={{width: '100px'}}>
+                        <img className="img-responsive" style="" src="data.gates[j].iconPath"/>
+                    </div>
+                    <h4><strong>data.gates[j].key</strong></h4>
+                    <p>data.gates[j].description</p>
+                    </li>
+                    );
+            }
+        }
     }
     return table
 }
