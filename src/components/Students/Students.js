34612426a2bd2d22198278a5c5d6784fc296fe3b
timajmello <timjamello@icloud.com>
2019-10-22 16:22:20 -0400

    beautified student info panel, student bubbles now clickable, new sorting options

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -21,9 +21,9 @@ export default class Students extends React.Component {
     render() {
         console.log(this.props.everything);
         return (
-            <div className="container">
+            <div className="container" style={{width: '95%'}}>
                 <div className="row">
-                    <div className="jumbotron" style={{height: '800px'}}>
+                    <div className="jumbotron" style={{height: '800px', paddingLeft: 0, paddingRight: 0}}>
                         <StudentFeedback student = {this.props.everything.students[this.state.selectedStudent]}/>
                         <StudentsTable everything = {this.props.everything} studentSwitch = {this.studentSwitch}/>
                         <StudentsPanel student = {this.props.everything.students[this.state.selectedStudent]}/>
