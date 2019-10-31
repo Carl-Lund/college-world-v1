c87208532d8ad8622718b37636f9380f303a9da8
Kena <kena.henry614@gmail.com>
2019-10-31 15:35:54 -0400

    small css changes

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -31,7 +31,7 @@ export default class Store extends React.Component {
                 // user cant afford item
                 if (this.props.everything.college.availableCash < this.props.everything.store[i].cost) {
                     itemTile.push(<div align="center" className="col-sm-4">
-                        <div className="well">
+                        <div className="well store-tile">
                             <img src={"resources/images/" + this.props.everything.store[i].imageName} height="50"
                                  width="50"></img>
                             <h4>{this.props.everything.store[i].name}</h4>
@@ -45,12 +45,12 @@ export default class Store extends React.Component {
                 else if (this.props.everything.store[i].gateNum > this.props.everything.college.gate){
 
                     itemTile.push(<div align="center" className="col-sm-4">
-                        <div className="well">
+                        <div className="well store-tile">
                             <img src={"resources/images/" + this.props.everything.store[i].imageName} height="50"
                                  width="50"></img>
                             <h4>{this.props.everything.store[i].name}</h4>
                             <h5>{this.props.everything.store[i].description}</h5>
-                            <p class="text-danger">${this.props.everything.store[i].cost}</p>
+                            <p class="text-muted">${this.props.everything.store[i].cost}</p>
                             <input type="submit" className="btn btn-info" name="buyItem" value={"Locked Until Level " +this.props.everything.store[i].gateNum} disabled></input>
                         </div>
                     </div>)
@@ -58,19 +58,19 @@ export default class Store extends React.Component {
                 // already purchased
                 else if(this.props.everything.store[i].isPurchased){
                     itemTile.push(<div align="center" className="col-sm-4">
-                        <div className="well">
+                        <div className="well store-tile">
                             <img src={"resources/images/" + this.props.everything.store[i].imageName} height="50"
                                  width="50"></img>
                             <h4>{this.props.everything.store[i].name}</h4>
                             <h5>{this.props.everything.store[i].description}</h5>
-                            <p class="text-danger">${this.props.everything.store[i].cost}</p>
+                            <p class="text-muted">${this.props.everything.store[i].cost}</p>
                             <input type="submit" className="btn btn-info" name="buyItem" value="Purchased" disabled></input>
                         </div>
                     </div>)
                 }
                 else{
                     itemTile.push(  <div align="center" className="col-sm-4">
-                        <div className="well">
+                        <div className="well store-tile">
                             <img src={"resources/images/" +this.props.everything.store[i].imageName} height="50" width="50"></img>
                             <h4>{this.props.everything.store[i].name}</h4>
                             <h5>{this.props.everything.store[i].description}</h5>
@@ -82,7 +82,9 @@ export default class Store extends React.Component {
                 allItems.push(itemTile)
             }
             return(
-                <div>
+                <div class="store-main">
+                    <h2 class="store-header">School Budget: ${this.props.everything.college.availableCash}</h2>
+
                     {allItems}
                 </div>
             );
@@ -90,7 +92,7 @@ export default class Store extends React.Component {
     }catch(error)
        {
             return(
-               <div>
+               <div class="store-main">
                     IM BROKEN.
                 </div>
             );
