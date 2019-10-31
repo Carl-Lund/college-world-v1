4b1fccddcd3a94e05f0cda456a2680da1ab42950
ebawmann <ebawm372@mail.endicott.edu>
2019-10-31 14:09:04 -0400

    Added glyphicons for objectives that are completed

--- src/components/Objectives/Objectives.js
+++ src/components/Objectives/Objectives.js
@@ -12,6 +12,8 @@ export default class Objectives extends React.Component {
                 <CurrentLevel everything={this.props.everything}/>
                 <h3> Current Objectives: </h3>
                 <CurrentObjectives everything={this.props.everything}/>
+                <h3> Completed Objectives: </h3>
+                <CompletedObjectives everything={this.props.everything}/>
             </div>
 
         )
