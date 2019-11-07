f4ab7e747161e25cc4b7280d6e35529ffe833467
Kena <kena.henry614@gmail.com>
2019-11-07 15:27:42 -0500

    ehhhhhhh matched it as close as i could

--- src/components/CollegeDashboard/CollegeDashboard.js
+++ src/components/CollegeDashboard/CollegeDashboard.js
@@ -10,12 +10,13 @@ export default class CollegeDashboard extends React.Component {
     render() {
         return (
             <div className="container">
+                <CollegeHappiness everything={this.props.everything}/>
+                {/*<StudentStatistics everything={this.props.everything}/>*/}
+                <CollegeRetention everything={this.props.everything}/>
                 <div className="row">
-                    <CollegeHappiness everything={this.props.everything}/>
-                    <CollegeRetention everything={this.props.everything}/>
-                    {/*<StudentStatistics everything={this.props.everything}/>*/}
-                    {/*<Tuition everything={this.props.everything} replaceEverything={this.props.replaceEverything}/>*/}
-                    {/*<PlayMode everything={this.props.everything} replaceEverything={this.props.replaceEverything}/>*/}
+                    <div className="col-sm">
+                        <PlayMode everything={this.props.everything} replaceEverything={this.props.replaceEverything}/>
+                    </div>
                 </div>
             </div>
         );
