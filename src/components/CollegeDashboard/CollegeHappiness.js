f2ed343276235b968c4f137b7f1045f2de3d43ba
ebawmann <ebawm372@mail.endicott.edu>
2019-11-21 13:31:47 -0500

    Circular progress bars now show colors!

--- src/components/CollegeDashboard/CollegeHappiness.js
+++ src/components/CollegeDashboard/CollegeHappiness.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default class CollegeHappiness extends React.Component {
 
     render() {
 
+        function getColor(value) {
+            let temp;
+            temp='#4287f5';
+            if (value>=60)
+                temp='#37db45';
+            else if (value<=49)
+                temp='#fc3d17';
+            else if (value<=59)
+                temp='#fce517';
+
+            return temp;
+        }
+
+        let color=getColor(this.props.everything.college.studentBodyHappiness);
+
         return (
                 <div className="col-sm-2">
                     <div>
                         <h3>Happiness</h3>
-                        <CircularProgressbar value={this.props.everything.college.studentBodyHappiness} text={this.props.everything.college.studentBodyHappiness} strokeWidth={15} />
+                        <CircularProgressbar value={this.props.everything.college.studentBodyHappiness} text={this.props.everything.college.studentBodyHappiness} strokeWidth={15} styles={buildStyles({pathColor: color, textColor: color})}/>
                         <br/>
                         {/*<button type="button" href="#happinessDetails" className="btn btn-light"*/}
                         {/*        data-toggle="collapse">Details*/}
