2f93c9486df876fadb4e0344d0f922dc259e5620
ebawmann <ebawm372@mail.endicott.edu>
2019-11-21 13:53:02 -0500

    Added progress bars for buildings, financial happiness, recreational happiness and student health (unfinished)

--- src/components/CollegeDashboard/CollegeDashboard.js
+++ src/components/CollegeDashboard/CollegeDashboard.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import CollegeHappiness from "../CollegeDashboard/CollegeHappiness";
 import CollegeRetention from "../CollegeDashboard/CollegeRetention";
+import CollegeBuildings from "../CollegeDashboard/CollegeBuildings";
+import CollegeFinancialHappiness from "../CollegeDashboard/CollegeFinancialHappiness";
+import CollegeRecreationalHappiness from "../CollegeDashboard/CollegeRecreationalHappiness";
+import CollegeStudentHealth from "../CollegeDashboard/CollegeStudentHealth";
 import StudentStatistics from "../CollegeDashboard/StudentStatistics";
 import Tuition from "../College/Tuition";
 import PlayMode from "../College/PlayMode";
@@ -12,10 +16,10 @@ export default class CollegeDashboard extends React.Component {
             <div className="container">
                 <CollegeHappiness everything={this.props.everything}/>
                 <CollegeRetention everything={this.props.everything}/>
-                <CollegeHappiness everything={this.props.everything}/>
-                <CollegeRetention everything={this.props.everything}/>
-                <CollegeHappiness everything={this.props.everything}/>
-                <CollegeRetention everything={this.props.everything}/>
+                <CollegeBuildings everything={this.props.everything}/>
+                <CollegeStudentHealth everything={this.props.everything}/>
+                <CollegeRecreationalHappiness everything={this.props.everything}/>
+                <CollegeFinancialHappiness everything={this.props.everything}/>
             </div>
         );
     }
---
+++ src/components/CollegeDashboard/CollegeRecreationalHappiness.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+export default class CollegeRecreationalHappiness extends React.Component {
+
+    render() {
+
+        function getColor(value) {
+            let temp;
+            temp='#4287f5';
+            if (value>=60)
+                temp='#37db45';
+            else if (value<=49)
+                temp='#fc3d17';
+            else if (value<=59)
+                temp='#fce517';
+
+            return temp;
+        }
+
+        let color=getColor(33);
+
+        return (
+            <div className="col-sm-2">
+                <div>
+                    <h3>Recreational Happiness</h3>
+                    <CircularProgressbar value={33} text={33} strokeWidth={15} styles={buildStyles({pathColor: color, textColor: color})}/>
+                    <br/>
+                    {/*<button type="button" href="#happinessDetails" className="btn btn-light"*/}
+                    {/*        data-toggle="collapse">Details*/}
+                    {/*</button>*/}
+                    <div id="happinessDetails" className="collapse">
+                        The happiness of the students depends on their health, academic success,
+                        tuition bills, and how much fun they are having.
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
