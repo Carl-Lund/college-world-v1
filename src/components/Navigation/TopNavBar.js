f44640d1c2c08ed2787e2844f937732425486dc8
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-11-05 04:40:43 +0530

    This is the react component of the top bar that is drawn on each page. It currently displays the college name, net balance, seats occupied, student faculty ratio on the left. It has a notification button that pops up the notification modal. On the right it has the number of days left in the semester and a button that jumps to the next day.

--- src/components/Navigation/TopNavBar.js
+++ src/components/Navigation/TopNavBar.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-// import Badge from '@material-ui/core/Badge';
-// import MailIcon from '@material-ui/icons/Mail';
+import Button from "react-bootstrap/Button";
+import Notification from "../Notifications/Notifications";
 
-export default class SideNavBar extends React.Component {
+export default class TopNavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.fetchData = this.fetchData.bind(this);
+        this.state = {appear : false};
+        this.setShow = (value) => {this.setState({appear:value})}
+        // const [show, setShow] = useState(false);
+        this.handleClose = () => this.setShow(false);
+        this.handleShow = () => this.setShow(true);
+    }
+
+    fetchData() {
+        const address = 'http://localhost:8080/enccollegeworld_war_exploded/rest/college/' + this.props.everything.college.runId + '/nextDay';
+        fetch(address)
+            .then(response => response.json())
+            .then(data => {this.props.replaceEverything(data)});
+    }
 
     render() {
-        let collegeName = "PlaceHolder";
-        // let collegeName = this.props.everything.collegeName;
+        let collegeName = this.props.collegeName;
+        let currentBalance = this.props.everything.college.availableCash.toLocaleString();
+        let studentFacultyRatio = this.props.everything.college.studentFacultyRatioRating;
+        let timeUntilNextSemester = 75 - Math.round((this.props.everything.college.hoursAlive - 1) / 24 + 1);
+        let totalStudents = this.props.everything.college.numberStudentsAdmitted;
 
         return (
             <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
@@ -18,22 +35,30 @@ export default class SideNavBar extends React.Component {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link>Total Balance</Nav.Link>
-                        <Nav.Link>Seats Occupied</Nav.Link>
-                        <Nav.Link>Student Faculty Ratio</Nav.Link>
-                        <Nav.Link><button type="button" className="btn btn-primary">
-                            Notifications <span className="badge badge-light">4</span>
-                        </button></Nav.Link>
-                        {/*<NavDropdown title="Notifications" id="basic-nav-dropdown">*/}
-                        {/*    <NavDropdown.Item href="#action/3.1">Message 1</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Item href="#action/3.2">Message 2</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Item href="#action/3.3">Message 3</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Divider />*/}
-                        {/*    <NavDropdown.Item href="#action/3.4">*/}
-                            {/*</NavDropdown.Item>*/}
-                        {/*</NavDropdown>*/}
-                        <Nav.Link href="#link">Time Until Next Semester</Nav.Link>
+                        <Nav.Link>Net Balance: <br />
+                            ${currentBalance}</Nav.Link>
+                        <Nav.Link>Seats Occupied: <br />
+                            {totalStudents}</Nav.Link>
+                        <Nav.Link>Student Faculty Ratio:<br />
+                            {studentFacultyRatio}</Nav.Link>
+                        <Nav.Link >
+                            {/*() => this.setShow(true)*/}
+                            {/*() => this.setShow(false)*/}
+                            <Button onClick={this.handleShow} variant="primary">Notifications <span className="badge badge-light">4</span></Button>
+                            <Notification show={this.state.appear} handleClose={this.handleClose} />
+                        </Nav.Link>
+
                     </Nav>
+                </Navbar.Collapse>
+            <Navbar.Collapse class = "justify-content-end">
+                <Nav>
+                    <Nav.Link href="#link">Time Until Next Semester:<br />
+                        {timeUntilNextSemester}
+                    </Nav.Link>
+                    <Nav.Link >
+                        <Button onClick={this.fetchData} variant="success">Jump To Next Day</Button>
+                    </Nav.Link>
+                </Nav>
                 </Navbar.Collapse>
             </Navbar>
         );
