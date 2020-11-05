e59e90af111334bb8973a942db887c86e0409580
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-11-06 01:43:41 +0530

    Added a Modal for a Calendar

---
+++ src/components/Navigation/NavigationModals/Calendar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default class Calendar extends React.Component {
+    render() {
+
+        return (
+            <Modal show={this.props.show} onHide={this.props.handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                    aria-labelledby="contained-modal-title-vcenter">
+                <Modal.Header closeButton>
+                    <Modal.Title  id="contained-modal-title-vcenter">
+                        Calendar
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="show-grid">
+                    <Container>
+                        <Row>
+                            <Col className="col-sm-6">Date</Col>
+                            <Col className="col-sm-6">Some Event</Col>
+                        </Row>
+                        <Row>
+                            <Col className="col-sm-6">Date</Col>
+                            <Col className="col-sm-6">Some Event</Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.props.handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+}
