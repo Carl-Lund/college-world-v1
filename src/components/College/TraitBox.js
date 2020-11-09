5857f57c857d05d1e1ce962f15bf017f761ad660
tdang131 <tdang987@mail.endicott.edu>
2020-11-09 16:13:30 -0500

    fixed the traitbox highlights

--- src/components/College/TraitBox.js
+++ src/components/College/TraitBox.js
@@ -7,15 +7,20 @@ class TraitBox extends Component{
     render(){
         function getColor(value) {
             let temp;
+            console.log(value);
             if (value>=70)
                 temp='success';
-            else if (value<=66)
+            else if (value<=40)
                 temp='danger';
-            else if (value<=33)
+            else if (value<=69)
                 temp='warning';
+
+            console.log(temp);
             return temp;
         }
         let color=getColor(this.props.rating);
+        // console.log(this.props.rating);
+        // console.log(color);
         return(
             <Card className="traitBox" border={color}>
                 <Card.Header className="label">{this.props.label}</Card.Header>
