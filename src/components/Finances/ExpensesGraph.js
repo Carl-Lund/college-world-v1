c1b754b38af9858808d3440444ed514cc62a2826
mpatti7 <61565709+mpatti7@users.noreply.github.com>
2020-11-19 14:40:23 -0500

    If there is no data in the expenses graph, a message displays saying so.

--- src/components/Finances/ExpensesGraph.js
+++ src/components/Finances/ExpensesGraph.js
@@ -7,29 +7,39 @@ export default class ExpensesGraph extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    createChart(data){
+        if(data.loansPercent === 0 && data.sportsPercent === 0 && data.buildingsPercent === 0
+            && data.facultyPercent === 0 && data.storePercent === 0){
+            return <p>Theres no data</p>;
+        }
+        else{
+           return <Chart
+                width={'400px'}
+                height={'300px'}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    ['Task', 'Hours per Day'],
+                    ['Loans', data.loansPercent],
+                    ['Sports', data.sportsPercent],
+                    ['Buildings', data.buildingsPercent],
+                    ['Faculty', data.facultyPercent],
+                    ['Store', data.storePercent],
+                ]}
+                options={{
+                    title: '',
+                }}
+                rootProps={{ 'data-testid': '1' }}
+                />
+        }
+    }
     render() {
 
         return (
             <div className="finance-col1-row2">
                 <h3>Expenses</h3>
-                <Chart
-                    width={'400px'}
-                    height={'300px'}
-                    chartType="PieChart"
-                    loader={<div>Loading Chart</div>}
-                    data={[
-                        ['Task', 'Hours per Day'],
-                        ['Loans', this.props.everything.college.expensesGraph.loansPercent],
-                        ['Sports', this.props.everything.college.expensesGraph.sportsPercent],
-                        ['Buildings', this.props.everything.college.expensesGraph.buildingsPercent],
-                        ['Faculty', this.props.everything.college.expensesGraph.facultyPercent],
-                        ['Store', this.props.everything.college.expensesGraph.storePercent],
-                    ]}
-                    options={{
-                        title: '',
-                    }}
-                    rootProps={{ 'data-testid': '1' }}
-                />
+                {this.createChart(this.props.everything.college.expensesGraph)}
             </div>
         );
     }
