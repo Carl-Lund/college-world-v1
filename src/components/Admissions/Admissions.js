ea72f0b7f1ead57ed8ffdd99ce6046c78ac0ff92
Matthew Cruz <mcruz772@mail.endicott.edu>
2020-11-30 18:18:20 -0500

    Added new Icon in Admissions.js Added Top panel with various info in Admissions.js

--- src/components/Admissions/Admissions.js
+++ src/components/Admissions/Admissions.js
@@ -32,10 +32,63 @@ export default class Admissions extends React.Component {
     }
 
     render() {
+        let availableBeds = 0;
+        let takenBeds = 0;
+        let availablePlates = 0;
+        let takenPlates = 0;
+        let availableDesks = 0;
+        let takenDesks = 0;
+        for(let i = 0; i < this.props.everything.buildings.length; i++) {
+            if (this.props.everything.buildings[i].isBuilt && this.props.everything.buildings[i].kindOfBuilding == "DORM") {
+                availableBeds += this.props.everything.buildings[i].capacity - this.props.everything.buildings[i].numStudents;
+                takenBeds += this.props.everything.buildings[i].numStudents;
+            }
+            if (this.props.everything.buildings[i].isBuilt && this.props.everything.buildings[i].kindOfBuilding == "DINING") {
+                availablePlates += this.props.everything.buildings[i].capacity - this.props.everything.buildings[i].numStudents;
+                takenPlates += this.props.everything.buildings[i].numStudents;
+            }
+            if (this.props.everything.buildings[i].isBuilt && this.props.everything.buildings[i].kindOfBuilding == "ACADEMIC") {
+                availableDesks += this.props.everything.buildings[i].capacity - this.props.everything.buildings[i].numStudents;
+                takenDesks += this.props.everything.buildings[i].numStudents;
+            }
+        }
+        let totalPotentialStudents = this.props.everything.admissions.groupA.length +
+            this.props.everything.admissions.groupB.length +
+            this.props.everything.admissions.groupC.length;
         return(
             <div>
-                <Application student = {this.state.currentStudent} show={this.state.applicationAppear} handleClose={this.handleCloseApplication} />
                 <h2 className="header">Admissions</h2>
+                    <div className="row m-2">
+                        <div className="col-sm text-center topbar navbar navbar-default navbar-fixed-top">
+                            <img className="topbar-icon" src="resources/images/college_world_icons/total_students.png"></img>
+                            <div className="topbar-text">
+                                <h4><strong>{this.props.everything.college.studentsGraduating}</strong></h4>
+                                <h6>Graduating Students</h6>
+                            </div>
+                        </div>
+                        <div className="col-sm text-center topbar">
+                            <img className="topbar-icon" src="resources/images/student.png"></img>
+                            <div className="topbar-text">
+                                <h4><strong>{this.props.everything.college.numberStudentsAdmitted}</strong></h4>
+                                <h6>Total Enrolled Students</h6>
+                            </div>
+                        </div>
+                        <div className="col-sm text-center topbar">
+                            <img className="topbar-icon" src="resources/images/presentation.png"/>
+                            <div className="topbar-text">
+                                <h4><strong>{this.props.everything.admissions.openCapacity}</strong></h4>
+                                <h6>Currently Open Space</h6>
+                            </div>
+                        </div>
+                        <div className="col-sm text-center topbar" id="last-bar">
+                            <img className="topbar-icon" src="resources/images/college_world_icons/question_mark.png"></img>
+                            <div className="topbar-text">
+                                <h4><strong>{totalPotentialStudents}</strong></h4>
+                                <h6>Students Considering</h6>
+                            </div>
+                        </div>
+                    </div>
+                <Application student = {this.state.currentStudent} show={this.state.applicationAppear} handleClose={this.handleCloseApplication} />
                 <div className="card-deck my-3">
                     <CollegeHappiness everything={this.props.everything}/>
                     <CollegeRetention everything={this.props.everything}/>
