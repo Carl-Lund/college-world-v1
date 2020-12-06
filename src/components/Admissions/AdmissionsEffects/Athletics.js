0521d31d4b8b3198cb7dd53e29875debfb36128c
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-12-06 17:12:06 +0530

    Now uses a stacked circular bar instead of a single circular bar.

--- src/components/Admissions/AdmissionsEffects/Athletics.js
+++ src/components/Admissions/AdmissionsEffects/Athletics.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CircularProgressbar, buildStyles, CircularProgressbarWithChildren, } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import StackedProgressbar from "./StackedProgressBar";
 
 export default class Athletics extends React.Component {
 
@@ -26,28 +27,37 @@ export default class Athletics extends React.Component {
         let color=getColor(this.props.impact);
 
         return (
+            // <div className="col-sm-2">
+            //     <div className="collegeHappinessBar">
+            //         <h3 className="text-center">ATHLETICS</h3>
+            //         <br></br>
+            //         <CircularProgressbarWithChildren value={this.props.impact} styles={buildStyles({pathColor: color})}>
+            //             {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+            //             <img
+            //                 style={{ width: 90, marginTop: -5 }}
+            //                 src="resources/images/athletictrait.png"
+            //             />
+            //             <div style={{ fontSize: 12}}>
+            //                 <strong>{Math.round(this.props.impact)}%</strong>
+            //             </div>
+            //         </CircularProgressbarWithChildren>
+            //         <br/>
+            //         {/*<button type="button" href="#happinessDetails" className="btn btn-light"*/}
+            //         {/*        data-toggle="collapse">Details*/}
+            //         {/*</button>*/}
+            //         <div id="happinessDetails" className="collapse">
+            //             The happiness of the students depends on their health, academic success,
+            //             tuition bills, and how much fun they are having.
+            //         </div>
+            //     </div>
+            // </div>
+
             <div className="col-sm-2">
                 <div className="collegeHappinessBar">
                     <h3 className="text-center">ATHLETICS</h3>
                     <br></br>
-                    <CircularProgressbarWithChildren value={this.props.impact} styles={buildStyles({pathColor: color})}>
-                        {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                        <img
-                            style={{ width: 90, marginTop: -5 }}
-                            src="resources/images/athletictrait.png"
-                        />
-                        <div style={{ fontSize: 12}}>
-                            <strong>{Math.round(this.props.impact)}%</strong>
-                        </div>
-                    </CircularProgressbarWithChildren>
+                    <StackedProgressbar image={"resources/images/athletictrait.png"} impact={this.props.impact} oldRating={this.props.oldRating}/>
                     <br/>
-                    {/*<button type="button" href="#happinessDetails" className="btn btn-light"*/}
-                    {/*        data-toggle="collapse">Details*/}
-                    {/*</button>*/}
-                    <div id="happinessDetails" className="collapse">
-                        The happiness of the students depends on their health, academic success,
-                        tuition bills, and how much fun they are having.
-                    </div>
                 </div>
             </div>
         );
