f01f5471653b1e865e9126c928349ed1e3c9e3bf
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-12-07 23:51:13 +0530

    Changed the Color scheming of the admission stat bars and impact value text.

--- src/components/Admissions/AdmissionsEffects/AcademicsStatBar.js
+++ src/components/Admissions/AdmissionsEffects/AcademicsStatBar.js
@@ -3,7 +3,6 @@ import { CircularProgressbar, buildStyles, CircularProgressbarWithChildren, } fr
 import StackedProgressbar from './StackedProgressBar';
 import 'react-circular-progressbar/dist/styles.css';
 import styles from "./ProgressBar.css";
-//https://codesandbox.io/s/stacked-circular-progressbars-using-react-circular-progressbar-forked-1kn1b?file=/index.css:0-1667
 export default class AcademicsStatBar extends React.Component {
 
     render() {
@@ -14,15 +13,6 @@ export default class AcademicsStatBar extends React.Component {
                     <h3 className="text-center">ACADEMICS</h3>
                     <br></br>
                     <StackedProgressbar image={"resources/images/academictrait.png"} impact={this.props.impact} oldRating={this.props.oldRating}/>
-                    {/*<CircularProgressbarWithChildren value={this.props.impact} styles={buildStyles({pathColor: color})}>*/}
-                    {/*    <img*/}
-                    {/*        style={{ width: 90, marginTop: -5 }}*/}
-                    {/*        src="resources/images/academictrait.png"*/}
-                    {/*    />*/}
-                    {/*    <div style={{ fontSize: 12}}>*/}
-                    {/*        <strong>{Math.round(this.props.impact)}%</strong>*/}
-                    {/*    </div>*/}
-                    {/*</CircularProgressbarWithChildren>*/}
                     <br/>
                 </div>
             </div>
