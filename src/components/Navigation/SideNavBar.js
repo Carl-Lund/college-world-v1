e8438be7c4eb20551d240ab70bd24e96c8b4423a
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-12-15 21:17:35 +0530

    Removed unused imports and added alt prop to images

--- src/components/Navigation/SideNavBar.js
+++ src/components/Navigation/SideNavBar.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
-import Notification from "./NavigationModals/Notifications";
 import './Navigation.css';
-import TopNavBar from "./TopNavBar";
 
 export default class SideNavBar extends React.Component {
 
@@ -20,73 +14,73 @@ export default class SideNavBar extends React.Component {
                             {/*<li className="nav-item"><Link to='/launch'>Launch</Link></li>*/}
                             <li className="nav-item">
                                 <NavLink to='/college' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/dashboard_information_bar.png"></img>
+                                    <img alt="dashboardInformationBar" className="nav-item-icons" src="resources/images/college_world_icons/dashboard_information_bar.png"></img>
                                     Dashboard
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/schooltraits' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/about__information_bar.png"></img>
+                                    <img alt="traitsImage" className="nav-item-icons" src="resources/images/college_world_icons/about__information_bar.png"></img>
                                     School Traits
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/students' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/students_information_bar.png"></img>
+                                    <img alt="studentImage" className="nav-item-icons" src="resources/images/college_world_icons/students_information_bar.png"></img>
                                     Students
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/admissions' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/admission.png"></img>
+                                    <img alt="admissionsImage"  className="nav-item-icons" src="resources/images/college_world_icons/admission.png"></img>
                                     Admissions
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/building' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/buildings_information_bar.png"></img>
+                                    <img alt="buildingsImage" className="nav-item-icons" src="resources/images/college_world_icons/buildings_information_bar.png"></img>
                                     Buildings
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/sports' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/sports_information_bar.png"></img>
+                                    <img alt="sportsImage"  className="nav-item-icons" src="resources/images/college_world_icons/sports_information_bar.png"></img>
                                     Sports
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/faculty' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/students_information_bar.png"></img>
+                                    <img alt="facultyImage" className="nav-item-icons" src="resources/images/college_world_icons/students_information_bar.png"></img>
                                     Faculty
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/finances' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/dollar_character.png" height="27"></img>
+                                    <img alt="financesImage"  className="nav-item-icons" src="resources/images/college_world_icons/dollar_character.png" height="27"></img>
                                     Finances
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/objectives' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/objectives_information_bar.png"></img>
+                                    <img alt="objectivesImage"  className="nav-item-icons" src="resources/images/college_world_icons/objectives_information_bar.png"></img>
                                     Objectives
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/store' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/store_information_bar.png"></img>
+                                    <img alt="storeImage"  className="nav-item-icons" src="resources/images/college_world_icons/store_information_bar.png"></img>
                                     Store
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/about' activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/about__information_bar.png"></img>
+                                    <img alt="aboutImage"  className="nav-item-icons" src="resources/images/college_world_icons/about__information_bar.png"></img>
                                     About
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to='/launch' onClick={this.props.doExit} activeClassName="active">
-                                    <img className="nav-item-icons" src="resources/images/college_world_icons/exit_information_bar.png"></img>
+                                    <img alt="exitImage" className="nav-item-icons" src="resources/images/college_world_icons/exit_information_bar.png"></img>
                                     Exit
                                 </NavLink>
                             </li>
