c906eb084b44873248b529fdf8dbbe5dafcdc3fd
S-Noah <nsarge@mail.endicott.edu>
2022-09-27 13:36:45 -0400

    Fixed Tuition Input Error. Added logic to strop trailing zeros.

--- src/components/Finances/Tuition.js
+++ src/components/Finances/Tuition.js
@@ -8,12 +8,16 @@ export default class Tuition extends React.Component {
     }
 
     handleOnChange(e) {
-        if(e.target.value > 100000) {
-            e.target.value = 100000;
+        let value = e.target.value
+        if(!isNaN(value)) { // Checks if the incoming value is a number.
+            value = // Restricts number to 0-100000 also eliminates trailing zeros.
+                (value === "") ? 0 :
+                (value[0] === '0' && value.length > 1) ? value.slice(1) :
+                (value > 100000) ? 100000 : value;
+            this.props.everything.college.yearlyTuitionCost = value;
+            this.props.replaceEverything(this.props.everything);
+            this.updateCollegeOnServer(this.props.everything.college)
         }
-        this.props.everything.college.yearlyTuitionCost = e.target.value;
-        this.props.replaceEverything(this.props.everything);
-        this.updateCollegeOnServer(this.props.everything.college);
     }
 
     updateCollegeOnServer(college) {
